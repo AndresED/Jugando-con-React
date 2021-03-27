b6a448af5a67017a456890bca33a51d5195d4dc0
Andres Esquivel <andres30xed@gmail.com>
2021-03-27 11:10:19 -0500

    default props

--- 02-counter-app/src/PrimeraApp.js
+++ 02-counter-app/src/PrimeraApp.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './index.css';
 import PropTypes from 'prop-types';
-const PrimeraApp = ({nombre})=>{
+const PrimeraApp = ({nombre,subtitulo})=>{
     return (
     <>
         <h1> Hola mundo {nombre}</h1>
         <h2> ¿Como estas?</h2>
+        <p>{subtitulo}</p>
     </>
     );
 }
 PrimeraApp.propTypes ={
     nombre: PropTypes.string.isRequired,
+}
+PrimeraApp.defaultProps ={
+    subtitulo: 'Soy un subtitulo',
 }
 export default PrimeraApp;

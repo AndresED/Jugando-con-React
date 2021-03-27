67d5811307514c8db109024fc3d6f2937d781c23
Andres Esquivel <andres30xed@gmail.com>
2021-03-27 09:08:18 -0500

    hola mundo create-react-app

---
+++ 02-counter-app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const saludo = <h1> Hola mundo 23</h1>;
+const divRoot = document.querySelector("#root");
+ReactDOM.render(saludo,divRoot);
+console.log(saludo)

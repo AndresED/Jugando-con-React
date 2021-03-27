cfad98ae45dee2824633922070cb0f5cf6f625f1
Andres Esquivel <andres30xed@gmail.com>
2021-03-27 13:56:06 -0500

    UseState Hook

--- 02-counter-app/src/CounterApp.js
+++ 02-counter-app/src/CounterApp.js
@@ -1,9 +1,25 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import './index.css';
+import React,{useState} from 'react';
+
 const CounterApp = ({value}) =>{
+    const [counter, setCounter] = useState(value);
+    const handleAdd = ()=>{
+        setCounter(counter+1);
+    }
+    const handleSubstration = ()=>{
+        setCounter(counter-1);
+    }
+    const handleReset = ()=>{
+        setCounter(value);
+    }
     return (
         <>
         <h1>CounterApp</h1>
-        <h2>{value}</h2>
+        <h2>{counter}</h2>
+        <button onClick={handleAdd}>+</button>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleSubstration}>-</button>
         </>
     );
 }
--- 02-counter-app/src/index.js
+++ 02-counter-app/src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import CounterApp from './CounterApp';
 import ReactDOM from 'react-dom';
 const divRoot = document.querySelector("#root");
-ReactDOM.render(<CounterApp value={123}/>,divRoot);
+ReactDOM.render(<CounterApp value={0}/>,divRoot);

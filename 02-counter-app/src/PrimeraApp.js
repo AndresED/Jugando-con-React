8e447e37254effa3a475e7109e9f6d330dad9bcb
Andres Esquivel <andres30xed@gmail.com>
2021-03-27 10:54:27 -0500

    validando props con proptypes

--- 02-counter-app/src/PrimeraApp.js
+++ 02-counter-app/src/PrimeraApp.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './index.css';
-const PrimeraApp = (props)=>{
-    console.log(props)
-    const {nombre} = props
+import PropTypes from 'prop-types';
+const PrimeraApp = ({nombre})=>{
     return (
     <>
         <h1> Hola mundo {nombre}</h1>
         <h2> ¿Como estas?</h2>
     </>
     );
+}
+PrimeraApp.propTypes ={
+    nombre: PropTypes.string.isRequired,
 }
 export default PrimeraApp;
--- 02-counter-app/src/index.js
+++ 02-counter-app/src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import PrimeraApp from './PrimeraApp';
 import ReactDOM from 'react-dom';
 const divRoot = document.querySelector("#root");
-ReactDOM.render(<PrimeraApp nombre="Goku"/>,divRoot);
+ReactDOM.render(<PrimeraApp />,divRoot);

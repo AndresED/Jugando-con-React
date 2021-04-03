33d040d12b5db0f5e4df0c81264cbbc921b1d8bd
Andres Esquivel <andres30xed@gmail.com>
2021-04-03 15:37:07 -0500

    creando componente GifExpertApp

---
+++ 03-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css';
+export const GifExpertApp = () => {
+    return (
+        <>
+        <h2>GifExpertApp</h2>
+        <hr></hr>
+        </>
+    )
+}

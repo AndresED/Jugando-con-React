ce6e64f34771b4809627c040e11229cb334f2afb
Andres Esquivel <andres30xed@gmail.com>
2021-04-03 08:44:11 -0500

    Pruebas con async y await

---
+++ 02-counter-app/src/tests/base/11-async-await.test.js
@@ -0,0 +1,7 @@
+import { getImagen } from "../../base/11-async-await";
+describe('Pruebas con async y await',()=>{
+    test('Debe de retornar el url de la imagen',async ()=>{
+        const url = await getImagen();
+        console.log(url)
+    })
+});

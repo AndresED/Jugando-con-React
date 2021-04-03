49d6319c4849137ccea77ba4c1d868181b91f717
Andres Esquivel <andres30xed@gmail.com>
2021-04-03 13:11:50 -0500

    usando el metodo find para buscar elementos en el DOM y realizar validaciones respectivas

--- 02-counter-app/src/tests/PrimeraApp.test.js
+++ 02-counter-app/src/tests/PrimeraApp.test.js
@@ -9,4 +9,13 @@ describe('Pruebas sobre PrimeraApp',()=>{
         const wrapper = shallow(<PrimeraApp nombre={nombre}/>);
         expect(wrapper).toMatchSnapshot();
     });
+    test("debe de mostrar el subtitulo enviado por los props",()=>{
+        const nombre = 'Hola, soy Goku'
+        const subtitulo = 'Soy un pinche subtitulo';
+        const wrapper = shallow(<PrimeraApp nombre={nombre} subtitulo={subtitulo}/>);
+        const texto = wrapper.find('p').text();
+        expect(texto).toBe(subtitulo);
+        expect(wrapper).toMatchSnapshot();
+
+    });
 });

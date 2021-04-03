b575bd63ba983679d6381e3c3f64445b01507448
Andres Esquivel <andres30xed@gmail.com>
2021-04-03 13:01:02 -0500

    uso de los snatshops para validar componentes

--- 02-counter-app/src/tests/PrimeraApp.test.js
+++ 02-counter-app/src/tests/PrimeraApp.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {render} from '@testing-library/react';
 import PrimeraApp from '../PrimeraApp';
+import '@testing-library/jest-dom';
+import { shallow, mount, render } from 'enzyme';
 describe('Pruebas sobre PrimeraApp',()=>{
     test("debe de mostrar el mensaje 'Hola, soy Goku'",()=>{
         const saludo = 'Hola, soy Goku';
         const nombre = 'Goku';
-        const wrapper = render(<PrimeraApp nombre={nombre}/>);
-        expect(wrapper.getByText(saludo)).toBeInTheDocument();
+        const wrapper = shallow(<PrimeraApp nombre={nombre}/>);
+        expect(wrapper).toMatchSnapshot();
     });
 });
